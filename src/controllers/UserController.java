3362dd5bb190e20a8e3c07f33c59342a1e8e1fb5
ihzacky <114144242+ihzacky@users.noreply.github.com>
2024-12-08 00:08:10 +0700

    migrate sql query code to controller package

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,6 +1,17 @@
 package controllers;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import utils.Connect;
+
 public class UserController {
+	
+	private Connect db;
+	
+	public UserController() {
+		this.db = Connect.getInstance();
+	}
 
 	public void login(String Username, String Password) {
 		
@@ -14,12 +25,29 @@ public class UserController {
 		
 	}
 	
-	public boolean register(String Username, String Password, String Phone_Number, String Address) {
+	public boolean register(String Username, String Password, String Phone_Number, String Address, String Role) {
 		
 		boolean validate = checkAccountValidation(Username, Password, Phone_Number, Address);
 		if(validate == true) {
 			// code insert data to database
+			String query = "INSERT INTO users(username, password, phone, address, role) VALUES(?, ?, ?, ?, ?)";
 			
+			try (PreparedStatement pst = db.prepareStatement(query)){
+				
+				pst.setString(1, Username);
+				pst.setString(2, Password);
+				pst.setString(3, Phone_Number);
+				pst.setString(4, Address);
+				pst.setString(5, Role);
+				pst.executeUpdate();
+				
+				
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+			System.out.println("Insert OK");
 			return true;
 		}else {
 			return false;
